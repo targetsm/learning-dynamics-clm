7c828a91b3390dcb7f74a24de56c1a367ef820bf
targetsm <gabriel.gavrilas@gmail.com>
2024-07-11 10:43:40 +0200

    save kl

--- kl/test_on_time.sh
+++ kl/test_on_time.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 ulimit -m 20000000
 source $HOME/ma/alti/venv_alti/bin/activate
-workdir=/local/home/ggabriel/ma/models
+workdir=/local/home/ggabriel/ma/models/tl/wmt22frde
 cd $workdir/tm/checkpoints
 
-if [ ! -d analysis ]; then
-   mkdir -p analysis
-   cp checkpoint_*_*.pt analysis
-   cd analysis
-   for filename in ./*; do echo $filename; mv $filename ${filename##*_}; echo ${filename##*_}; done
-fi
+#if [ ! -d analysis ]; then
+#   mkdir -p analysis
+#   cp checkpoint_*_*.pt analysis
+#   cd analysis
+#   for filename in ./*; do echo $filename; mv $filename ${filename##*_}; echo ${filename##*_}; done
+#fi
 
-cd $workdir/lm_trunc/checkpoints
-if [ ! -d analysis ]; then
-   mkdir -p analysis
-   cp checkpoint_*_*.pt analysis
-   cd analysis
-   for filename in ./*; do mv $filename ${filename##*_}; done
-fi
+#cd $workdir/lm/checkpoints
+#if [ ! -d analysis ]; then
+#   mkdir -p analysis
+#   cp checkpoint_*_*.pt analysis
+#   cd analysis
+#   for filename in ./*; do mv $filename ${filename##*_}; done
+#fi
 
 cd $workdir
 mkdir -p evaluation
 
 for filename in $workdir/tm/checkpoints/analysis/*; do
-    filename=$(basename $filename .pt)
+    f=$(basename $filename .pt)
     echo $filename
-    CUDA_VISIBLE_DEVICES=3 fairseq-generate tm/data-bin/iwslt14.sep.tokenized.de-en \
-        --path tm/checkpoints/analysis/$filename.pt \
+    echo $f
+    CUDA_VISIBLE_DEVICES='' fairseq-generate tm/data-bin/wmt22.sep.tokenized.fr-de \
+        --path tm/checkpoints/analysis/$f.pt \
         --batch-size 256 --beam 1\
 	--results-path evaluation/tm \
         --score-reference
-    CUDA_VISIBLE_DEVICES=3 fairseq-generate lm_trunc/data-bin/iwslt14.sep.tokenized.de-en \
-        --path lm_trunc/checkpoints/analysis/$filename.pt \
+    CUDA_VISIBLE_DEVICES='' fairseq-generate lm/data-bin/wmt22.sep.tokenized.fr-de \
+        --path lm/checkpoints/analysis/$f.pt \
         --batch-size 256 --beam 1 \
 	--results-path evaluation/lm \
         --score-reference
@@ -47,7 +48,7 @@ for filename in $workdir/tm/checkpoints/analysis/*; do
     rm lm.txt lm.val
     
     cd $HOME/ma/kl
-    python -u kl_on_time_efficient.py $filename
+    python -u kl_on_time_efficient.py $f
     cd $workdir
     rm -rf evaluation/*
 done
