9865030a0dc9d413398d92bea78a8d7f15a49859
targetsm <gabriel.gavrilas@gmail.com>
2024-07-20 14:34:30 +0200

    Plot also standard error

--- alti/transformer-contributions-nmt-v2/plot.py
+++ alti/transformer-contributions-nmt-v2/plot.py
@@ -11,11 +11,21 @@ import matplotlib.pylab as plt
 lists = sorted(alti_dict.items()) # sorted by key, return a list of tuples
 
 x, y = zip(*lists) # unpack a list of pairs into two tuples
-y_src, y_tgt = zip(*y)
+#print(x)
+src = [el['src'] for el in y]
+trg = [el['trg'] for el in y]
+#print(src)
+#y_src, y_tgt = zip(*y)
+src_mean = [numpy.mean(el) for el in src]
+src_se = [numpy.std(el)/numpy.sqrt(len(src)) for el in src]
+trg_mean = [numpy.mean(el) for el in trg]
+trg_se = [numpy.std(el)/numpy.sqrt(len(trg)) for el in trg]
 
+#plt.plot(x, y_src, linestyle='-', marker='.', label='source')
+#plt.plot(x, y_tgt, linestyle='-', marker='.', label='target prefix')
+plt.errorbar(x, src_mean, src_se, capsize=4, linestyle='-', marker='.', label='source')
+plt.errorbar(x, trg_mean, trg_se, capsize=4, linestyle='-', marker='.', label='target prefix')
 
-plt.plot(x, y_src, linestyle='-', marker='.', label='source')
-plt.plot(x, y_tgt, linestyle='-', marker='.', label='target prefix')
 plt.legend()
 plt.title('ALTI+ mean source and target prefix contributions over the course of training', loc='center', wrap=True)
 plt.grid(True)
