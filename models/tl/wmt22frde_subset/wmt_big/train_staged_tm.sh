e3dd74fbc7fea88c90e266f5170172c4c9eb104c
targetsm <gabriel.gavrilas@gmail.com>
2024-08-05 15:31:04 +0200

    add train scripts and log

--- models/tl/wmt22frde_subset/wmt_big/train_staged_tm.sh
+++ models/tl/wmt22frde_subset/wmt_big/train_staged_tm.sh
@@ -1,66 +1,60 @@
 #!/bin/bash
-#while ps -p 3225959; do sleep 60; done
-
-ulimit -m 12000000
+ulimit -m 20000000
 source $HOME/ma/alti/venv_alti/bin/activate
-ORIG=/local/home/ggabriel/ma/data/tl/wmt22frde/wmt22.sep.tokenized.fr-de
-TEXT=./data/wmt22.sep.tokenized.fr-de
-
-# remove data from source file
-#mkdir -p data
-#cp -r $ORIG data
-#cd $TEXT
-
-#cp $HOME/ma/scripts/clean_lm_trunc.py .
-#exit 0
-#python clean_lm_trunc.py
-#cd ../../
+#TEXT=$HOME/ma/data/sentp/iwslt14.sep.tokenized.de-en
+TEXT=/local/home/ggabriel/ma/data/tl/wmt22frde/wmt22.sep.tokenized.fr-de
 
 #fairseq-preprocess --source-lang fr --target-lang de \
 #    --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
 #    --destdir data-bin/wmt22.sep.tokenized.fr-de \
 #    --workers 8
-#exit 0
-CUDA_VISIBLE_DEVICES=1 fairseq-train \
+
+CUDA_VISIBLE_DEVICES=7 fairseq-train \
     data-bin/wmt22.sep.tokenized.fr-de \
-    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --arch transformer_wmt_en_de_big --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
-    --no-epoch-checkpoints \
+    --maximize-best-checkpoint-metric --no-epoch-checkpoints \
     --save-interval-updates 100 --max-update 1000 
 
-CUDA_VISIBLE_DEVICES=1 fairseq-train \
+CUDA_VISIBLE_DEVICES=7 fairseq-train \
     data-bin/wmt22.sep.tokenized.fr-de \
-    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --arch transformer_wmt_en_de_big --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
-    --no-epoch-checkpoints \
+    --maximize-best-checkpoint-metric --no-epoch-checkpoints \
     --save-interval-updates 500 --max-update 10000
 
-CUDA_VISIBLE_DEVICES=1 fairseq-train \
+CUDA_VISIBLE_DEVICES=7 fairseq-train \
     data-bin/wmt22.sep.tokenized.fr-de \
-    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --arch transformer_wmt_en_de_big --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
-    --no-epoch-checkpoints \
+    --maximize-best-checkpoint-metric --no-epoch-checkpoints \
     --save-interval-updates 1000 --max-update 100000
 
 #CUDA_VISIBLE_DEVICES=0 fairseq-train \
-#    data-bin/iwslt14.sep.tokenized.de-en \
+#    data-bin/iwslt14.tokenized.de-en \
 #    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
 #    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
 #    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
 #    --dropout 0.3 --weight-decay 0.0001 \
 #    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
 #    --max-tokens 4096 \
-#    --no-epoch-checkpoints --disable-validation --no-last-checkpoints \
+#    --eval-bleu --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+#    --eval-bleu-detok moses --eval-bleu-remove-bpe --eval-bleu-print-samples \
+#    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
 #    --save-interval-updates 50000 --max-update 1000000
+#
+#fairseq-generate data-bin/iwslt14.sep.tokenized.de-en \
+#    --path checkpoints/checkpoint_best.pt \
+#    --batch-size 128 --beam 5 --remove-bpe
